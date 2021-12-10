4393e8c6063daa597967f6fb415593b60c059557
Rcuhi <rappinngu2006@gmail.com>
2021-12-10 13:13:15 +0900

    スマホでも動くように

--- MemoryGame/Assets/Plugins/OpenNewTab.jslib
+++
@@ -1,5 +0,0 @@
-mergeInto(LibraryManager.library, {
-    OpenNewTab : function(url) {
-        window.open(Pointer_stringify(url));
-    }
-});
--- MemoryGame/Assets/Runtime/Data/Web/Twitter.cs
+++ MemoryGame/Assets/Runtime/Data/Web/Twitter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+#if !UNITY_EDITOR && UNITY_WEBGL
 using System.Runtime.InteropServices;
+#endif
 using UnityEngine;
 
 /// <summary>
@@ -9,20 +11,19 @@ public static class Twitter
 {
     // 新しいタブでURLを開く
 #if !UNITY_EDITOR && UNITY_WEBGL
-    // WebGLビルドで有効になる
     [DllImport("__Internal")]
-    private static extern void OpenNewTab(string url);
+    private static extern string TweetFromUnity(string rawMessage);
 #else
     // UnityエディタやWebGL以外のプラットフォームで有効になる
-    private static void OpenNewTab(string url) => Application.OpenURL(url);
+    private static void TweetFromUnity(string url) => Application.OpenURL(url);
 #endif
-    
+
     // ツイート画面を開く
     public static void Tweet(string text, string linkUrl, string hashtags)
     {
         var url = "https://twitter.com/intent/tweet?" + "text=" + text + "&url=" + linkUrl + "&hashtags=" + hashtags;
         var uri = new Uri(url);
         // WebGLの場合は、ゲームプレイ画面と同じウィンドウでツイート画面が開かないよう、処理を変える
-        OpenNewTab($"window.open('{uri.AbsoluteUri}','_blank')");
+        TweetFromUnity($"window.open('{uri.AbsoluteUri}','_blank')");
     }
 }
